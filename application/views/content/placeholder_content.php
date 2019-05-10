7f56fe887a73dd4977be8c5aac61b1562666bf23
dustinbayer <dustinbayer@gmail.com>
2019-05-09 22:02:37 -0700

    project cleanup

--- application/views/content/placeholder_content.php
+++
@@ -1,7 +0,0 @@
-<!--------------------------------------------------------------------
-! PLACEHOLDER MESSAGE 
-!-------------------------------------------------------------------->
-
-<h1>In Development</h1>
-
-<a href="<?php echo base_url('') ?>" class="category">I make all changes live, so something should be here soon!</a>
