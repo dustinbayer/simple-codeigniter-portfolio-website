19f9bb6b3cf6c908cfff1eb73063e32323fdee51
dustinbayer <dustinbayer@gmail.com>
2019-05-11 18:09:32 -0700

    typo fix

--- application/views/content/treeview/PROJECTS/2019 PORTFOLIO WEBSITE/detail.php
+++ application/views/content/treeview/PROJECTS/2019 PORTFOLIO WEBSITE/detail.php
@@ -1,5 +1,5 @@
 <h2 class="title">Portfolio Website</h2>
 
-<h3 class="detail">Summary: This is my porfolio website! I've designed it to be informative, straightforward, fast, lightweight, and green.</h3>
+<h3 class="detail">Summary: This is my portfolio website! I've designed it to be informative, straightforward, fast, lightweight, and green.</h3>
 <h3 class="detail">Check it out on <a href="https://github.com/dustinbayer/simple-codeigniter-portfolio-website">GitHub</a></h3>
 <h3 class="detail">Tools: PHP, CodeIgniter, HTML, CSS, JavaScript, css-doodle </h3>
